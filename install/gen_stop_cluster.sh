f5e8342475883817170305cd92b120e3aefe925e
yangdaixing <yangdaixing@bonc.com.cn>
2016-03-31 17:06:28 +0800

    update

--- install/gen_stop_cluster.sh
+++ install/gen_stop_cluster.sh
@@ -11,6 +11,6 @@ do
      echo "start \$host"
      ssh -f -n  \${host} "cd \$install_path/sbin; sh stopall.sh > /dev/null 2>&1;"
 done
-echo "=====started cluster===="
+echo "=====stopped cluster===="
 EOF
 
