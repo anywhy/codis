98b41c5ef5ee63fcdca10b306e288da5208f965b
yangdaixing <yangdaixing@bonc.com.cn>
2016-04-26 10:52:52 +0800

    :.update match delete

--- cmd/cconfig/redis_manager.go
+++ cmd/cconfig/redis_manager.go
@@ -33,7 +33,7 @@ func delData(network string, valKey string) (int, error) {
 		log.Warning(err)
 	}
 
-	if !strings.HasSuffix(valKey, "*") {
+	if !strings.HasSuffix(valKey, "*") && !strings.HasPrefix(valKey, "*") {
 		// del data
 		_, err := conn.Do("DEL", valKey)
 		if err != nil {
